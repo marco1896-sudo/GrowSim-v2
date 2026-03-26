3d17d08eca19ecd256b0075899419e90ba30d020
marco1896-sudo <marco.tepass@gmail.com>
2026-03-26 12:38:00 +0100

    Refine climate hint and fix mobile panel scrolling

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'growsim-v1-20260326-climate-readout-fix-v1';
+const CACHE_VERSION = 'growsim-v1-20260326-climate-hint-refine-v1';
 const SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 const BASE_PATH = new URL(self.registration.scope).pathname.replace(/\/$/, '');
